fd5b1443a62966f42eb2a1f14bbf6ccfa56aa2d4
Chris <Chris.Inkpen@gmail.com>
2017-04-17 19:27:47 +0100

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy} from '@angular/core';
+import { PrimeGeneratorService } from './prime-generator.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [PrimeGeneratorService]
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   title = 'app works!';
+  primes: number[] = [];
+
+  constructor(private prime: PrimeGeneratorService) {
+    setTimeout(() =>
+      prime.primeFound.subscribe(prime => this.primes.unshift(prime)), 10000);
+
+  }
+
+  ngOnDestroy(){
+      this.prime.stop();
+  }
+
+  onStop(){
+    this.prime.stop();
+  }
 }
---
+++ src/app/prime-generator.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import { Observable, Subject,ReplaySubject } from 'rxjs'
+
+
+@Injectable()
+export class PrimeGeneratorService {
+  count = 1;
+  maxCount = 10000;
+  primeFound = new ReplaySubject<number>();
+  stopNow = false;
+
+  constructor() {
+    this.count = 1;
+  this.start();
+  }
+
+  start() {
+    this.count++;
+    if (this.stopNow)
+      return ;  
+
+      
+    if (this.count < this.maxCount) {
+
+      if (this.isPrime(this.count)) {
+       
+        this.primeFound.next(this.count);
+      }
+     
+      setTimeout(() => { this.start(); }, 1);
+    }
+  }
+
+  stop(){
+    this.stopNow  = true;
+  }
+
+
+  isPrime(aNumber: number): boolean {
+    if (aNumber == 2)
+      return true;
+    let half = aNumber / 2;
+ 
+ 
+    for (var i = 2; i <= half; i++) {
+      if (aNumber % i === 0) {
+       
+        return false;
+      }
+    }
+    return true;
+  }
+
+}
